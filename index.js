7ed2763f87edc8f6bef6cbc65566e9de1cee8385
aiwaya <aiwaya@gmail.com>
2019-12-10 08:47:58 +0900

    g

--- index.js
+++ index.js
@@ -16,38 +16,17 @@ app.use(express.json());
 const SECRET = 'IWAYAAKIHIRO';
 
 async function insert_data(serial_id, temperature, voltage, current, timestamp) {
-    try {
 
-        client.connect();
-
-
-        const query = {
-            text: 'INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);',
-            value: [serial_id, temperature, voltage, current, new Date(timestamp)]
-        };
-
-
-        client.query('INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);', [serial_id, temperature, voltage, current, new Date(timestamp)], (err, res) => {
-            if (err) throw err;
-            client.end();
+    client.connect();
+    client.query('INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);',
+        [serial_id, temperature, voltage, current, new Date(timestamp)], (err, res) => {
+            if (err) {
+                console.log(err);
+                return 0;
+            } else {
+                return 1;
+            }
         });
-
-/*
-        console.log(serial_id + temperature+voltage+current+timestamp);
-        await database.one('INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);',
-            [serial_id, temperature, voltage, current, new Date(timestamp)])
-            .then(data => {
-                return data.id;
-            })
-            .catch(error => {
-                console.log('ERROR:', error); // print error;
-            });
-            */
-
-    } catch (err) {
-        console.log('can not insert record')
-        console.error(err);
-    }
 }
 
 
@@ -55,9 +34,14 @@ app.post('/things/:serial_id/data', function(req, res) {
     try {
         let access_token = req.header('Authorization').split(' ')[1];
         console.log('access token : ' + access_token);
-        console.log(req.body);
-        insert_data(req.params.serial_id, req.body.temperature, req.body.voltage, req.body.current, req.body.timestamp);
-        res.json({id: 'a'});
+
+        let code = insert_data(req.params.serial_id, req.body.temperature, req.body.voltage, req.body.current, req.body.timestamp);
+        if(code == 0) {
+            res.status(500).send();
+        } else {
+            res.status(201).send();
+        }
+
     } catch (err) {
     }
 })
