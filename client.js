de05d769ded56d81e5967a05b185dcd791333967
aiwaya <aiwaya@gmail.com>
2019-12-10 08:20:28 +0900

    g

--- client.js
+++ client.js
@@ -1,26 +1,9 @@
-function get_token(thing_id, token) {
-    var buff = Buffer.from(thing_id + ':' + token);
-    var client_secret_base64 = buff.toString('base64');
 
-    var request = require('sync-request');
-    var res = request('POST', 'https://iotplatform-heroku.herokuapp.com/token', {
-        headers: {
-            'Authorization': 'Basic ' + client_secret_base64,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'grant_type=client_credentials'
-    });
-    res = JSON.parse(res.getBody('utf8'));
-
-    console.log(res);
-    return res.access_token;
-
-};
 
 
 function create_data(access_token, body) {
     var request = require('sync-request');
-    var res = request('POST', 'https://iotplatform-heroku.herokuapp.com/things/thing1/data', {
+    var res = request('POST', 'https://jig-saw-sfdc-sample.herokuapp.com/things/123/data', {
         headers: {
             'Authorization': 'Bearer ' + access_token,
             'Content-Type': 'application/json'
@@ -31,6 +14,6 @@ function create_data(access_token, body) {
 
 };
 
-let access_token = get_token('thing_1', 'asa');
-let body = '{"timestamp": "2019-12-23T18:25:43.511Z","value": 12.12}';
+let access_token = 'aaa';
+let body = '{"timestamp":"2019-12-23T18:25:43.511Z","temperature":12.12,"voltage":10.1,"current":20.1}';
 create_data(access_token, body);
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-const pgPromise = require('pg-promise')({noWarnings: true});
-const databaseURL = process.env.DATABASE_URL;
-const database = pgPromise(databaseURL);
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
 
 var app = express();
 app.use(express.urlencoded({extended: true}));
@@ -13,9 +17,24 @@ const SECRET = 'IWAYAAKIHIRO';
 
 async function insert_data(serial_id, temperature, voltage, current, timestamp) {
     try {
+
+        client.connect();
+
+
+        const query = {
+            text: 'INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);',
+            value: [serial_id, temperature, voltage, current, new Date(timestamp)]
+        };
+
+
+        client.query(query, (err, res) => {
+            if (err) throw err;
+            client.end();
+        });
+
+/*
         console.log(serial_id + temperature+voltage+current+timestamp);
         await database.one('INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);',
-
             [serial_id, temperature, voltage, current, new Date(timestamp)])
             .then(data => {
                 return data.id;
@@ -23,6 +42,8 @@ async function insert_data(serial_id, temperature, voltage, current, timestamp) 
             .catch(error => {
                 console.log('ERROR:', error); // print error;
             });
+            */
+
     } catch (err) {
         console.log('can not insert record')
         console.error(err);
