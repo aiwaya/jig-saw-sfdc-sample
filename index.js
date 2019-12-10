cf587426ad16aa9f81e0fc3ce627b61963475277
aiwaya <aiwaya@gmail.com>
2019-12-10 09:25:27 +0900

    g

--- index.js
+++ index.js
@@ -16,35 +16,29 @@ app.use(express.json());
 
 const SECRET = 'IWAYAAKIHIRO';
 
+
+
+
 function insert_data(serial_id, temperature, voltage, current, timestamp) {
-
-
     client.query('INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);',
         [serial_id, temperature, voltage, current, new Date(timestamp)], (err, res) => {
             if (err) {
-                console.log(err);
-                return 0;
+                res.status(500).send();
+
             } else {
-                return 1;
+                res.status(201).send();
             }
         });
 }
 
 
 app.post('/things/:serial_id/data', function(req, res) {
-    try {
+
         let access_token = req.header('Authorization').split(' ')[1];
         console.log('access token : ' + access_token);
 
-        let code = insert_data(req.params.serial_id, req.body.temperature, req.body.voltage, req.body.current, req.body.timestamp);
-        if(code == 0) {
-            res.status(500).send();
-        } else {
-            res.status(201).send();
-        }
+        insert_data(req.params.serial_id, req.body.temperature, req.body.voltage, req.body.current, req.body.timestamp);
 
-    } catch (err) {
-    }
 })
 
 
