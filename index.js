9f5a10b04ef76893ab350fef71f1d945d6510d06
aiwaya <aiwaya@gmail.com>
2019-12-10 10:10:52 +0900

    g

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/things/:serial_id/data', function(req, res) {
         return;
     }
 
-    let serial_id, temperature, voltage, current, timestamp;
+    var serial_id, temperature, voltage, current, timestamp;
     try {
         serial_id = req.params.serial_id;
         temperature = req.body.temperature;
@@ -42,7 +42,7 @@ app.post('/things/:serial_id/data', function(req, res) {
     }
 
     client.query('INSERT INTO data(serial_id, temperature, voltage, current, timestamp) VALUES($1,$2,$3,$4,$5);',
-        [serial_id, temperature, voltage, current, new Date()], (err, result) => {
+        [serial_id, temperature, voltage, current, new Date(timestamp)], (err, result) => {
             if (err) {
                 console.error(err);
                 res.status(500).send({error: err});
