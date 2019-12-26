9b09872f6306ce5abff6c83ec5fa169d7b3b6d39
aiwaya <aiwaya@gmail.com>
2019-12-26 13:40:33 +0900

    g

--- scripts/create_table.js
+++ scripts/create_table.js
@@ -1,5 +1,10 @@
-const connectionString = process.env.DATABASE_URL;
-const client = new pg.Client(connectionString);
+const {Client} = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
 
 client.connect();
 client.query('CREATE TABLE IF NOT EXISTS data (id SERIAL, v1 INTEGER, v2 TIMESTAMP, v3 CHAR(255), v3 CHAR(255), v4 CHAR(255), v5 CHAR(255), v6 CHAR(255), v7 CHAR(255), v8 CHAR(255), v9 CHAR(255), serial_no CHAR(255)', (err, res) => {
