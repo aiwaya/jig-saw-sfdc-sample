1aabeb07a92e9a85a55423cfb985135153a629b7
aiwaya <aiwaya@gmail.com>
2019-12-26 14:03:26 +0900

    g

--- scripts/create_table.js
+++ scripts/create_table.js
@@ -7,7 +7,7 @@ const client = new Client({
 
 
 client.connect();
-client.query('CREATE TABLE IF NOT EXISTS data (id SERIAL, v1 INTEGER, v2 TIMESTAMP, v3 CHAR(255), v3 CHAR(255), v4 CHAR(255), v5 CHAR(255), v6 CHAR(255), v7 CHAR(255), v8 CHAR(255), v9 CHAR(255), serial_no CHAR(255)', (err, res) => {
+client.query('CREATE TABLE IF NOT EXISTS data1 (id SERIAL, v1 INTEGER, v2 TIMESTAMP, v3 CHAR(255), v4 CHAR(255), v5 CHAR(255), v6 CHAR(255), v7 CHAR(255), v8 CHAR(255), v9 CHAR(255), serial_no CHAR(255))', (err, res) => {
     if (err) {
         return console.error('error with PostgreSQL database', err);
     }
