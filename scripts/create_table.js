5725eadc9cc55e62ffe479374df60fae66cecea9
aiwaya <aiwaya@gmail.com>
2019-12-26 13:36:48 +0900

    g

---
+++ scripts/create_table.js
@@ -0,0 +1,10 @@
+const connectionString = process.env.DATABASE_URL;
+const client = new pg.Client(connectionString);
+
+client.connect();
+client.query('CREATE TABLE IF NOT EXISTS data (id SERIAL, v1 INTEGER, v2 TIMESTAMP, v3 CHAR(255), v3 CHAR(255), v4 CHAR(255), v5 CHAR(255), v6 CHAR(255), v7 CHAR(255), v8 CHAR(255), v9 CHAR(255), serial_no CHAR(255)', (err, res) => {
+    if (err) {
+        return console.error('error with PostgreSQL database', err);
+    }
+    client.end();
+});
